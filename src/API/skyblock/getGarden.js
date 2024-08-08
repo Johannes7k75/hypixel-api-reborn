fbdc9f8216f77d9fc24428d146cf0588fd00e18a
Johannes7k75 <Jonashebenstreit1@gmail.com>
2024-08-09 00:26:26 +0200

    fix garden

--- src/API/skyblock/getGarden.js
+++ src/API/skyblock/getGarden.js
@@ -1,7 +1,10 @@
 module.exports = async function (profileId) {
   const SkyblockGarden = require('../../structures/SkyBlock/SkyblockGarden');
-  // eslint-disable-next-line no-underscore-dangle
-  const res = await this._makeRequest(`/skyblock/garden?profile=${profileId}`);
-  if (res.raw) return res;
+  let res;
+  try {
+    // eslint-disable-next-line no-underscore-dangle
+    res = await this._makeRequest(`/skyblock/garden?profile=${profileId}`);
+  } catch (e) {}
+  if (res?.raw) return res;
   return new SkyblockGarden(res);
 };
